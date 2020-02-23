5ee0f8c4bea3fb4358fa3107d9487951bff51231
KK <50294003+krtech3@users.noreply.github.com>
2020-02-23 23:28:48 +0900

    Create head_line_article_mailer.rb

---
+++ head_line_article_mailer.rb
@@ -0,0 +1,10 @@
+class HeadLineArticleMailer < ApplicationMailer
+  def notification(head_line_articles)
+    @head_line_articles = head_line_articles
+      subject = '今日の為替と株価'
+      from = '今日の為替と株価<info@w-bot.com>'
+      to = 'hogehoge@example.com'
+      mail(to: to, from: from, subject: subject)
+    end
+  end
+end
